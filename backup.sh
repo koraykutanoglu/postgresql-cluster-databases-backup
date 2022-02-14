2e2e04569fa56982305084e71251d8d91e25c4d4
enesbakis <48660542+enesbakis@users.noreply.github.com>
2022-02-14 03:25:44 +0300

    Update backup.sh
    separator definitions updated

--- backup.sh
+++ backup.sh
@@ -10,30 +10,31 @@ sftp_backup_directory=/root/backup
 keepalivedip=xx
 backup_time=5
 file_password=password
+separator=$(printf "%-73s" "-")
 
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Checking Keepalived IP"
 keepalived=$(hostname -I | grep -o $keepalivedip)
 if [[ "$keepalived" -eq "$keepalivedip" ]]; then
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "IP checked. Backup is progress"
 sleep 2
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Checking the date and Date is assigned as a variable"
 date=`date +%Y-%m-%d-%H-%M`
 sleep 2
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Create a directory for date $date"
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 mkdir -p $local_backup_directory/$date
 backupdir=$local_backup_directory/$date
 echo "PostgreSQL backup is start"
 sleep 2
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Backup directory: $backupdir"
 sleep 1
 start=$(date +%s)
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "pg_dumpall is start"
 sleep 2
 sudo -u postgres pg_dumpall --globals-only | gzip > $backupdir/postgres_globals.sql.gz
@@ -41,36 +42,36 @@ for db in `sudo -u postgres psql -t -c "select datname from pg_database where no
    sudo -u postgres pg_dump $db | gzip | gpg -c --batch --passphrase $file_password > $backupdir/$db.sql.gz.gpg
 done
 end=$(date +%s) 
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Backup is completed"
 sleep 2
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Backup time: $(($end-$start)) second"
 sleep 2
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Backup list:"
 ls $backupdir
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "SFTP connection is checking"
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 ssh -q -p $sftp_port root@$sftp_ip exit
 if [[ "$?" -eq "0" ]]; then
 echo "older than $backup_time days backup files in SFTP server the deleting"
 ssh -o StrictHostKeyChecking=no -p $sftp_port -l $sftp_user $sftp_ip "find $sftp_backup_directory/* -mtime +$backup_time -exec rm -rf {} \;"
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Connection is successful, file transfer is starting"
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Backup directory is creating in SFTP server"
 ssh -o StrictHostKeyChecking=no -l $sftp_user $sftp_ip "mkdir -p $sftp_backup_directory/$date"
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "Backup files transfer to SFTP server"
 scp -P $sftp_port $backupdir/* root@$sftp_ip:$sftp_backup_directory/$date
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 echo "File transfer is finished"
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 rm -rf $backupdir
 echo "Backup files are deleted"
-echo "-------------------------------------------------------------------------"
+echo "${separator// /-}"
 else
 echo "Connection is fail. Backup is stopping."
 fi
